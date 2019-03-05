de44e693ad42421d0dc152b910474d7a4c727005
Marcin Miazga <miazgamarcin@outlook.com>
2019-03-05 14:56:08 +0100

    update server to enable authorization

--- server/Server/Controllers/ValuesController.cs
+++ server/Server/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Server.Controllers
@@ -9,6 +10,7 @@ namespace Server.Controllers
     {
         // GET api/values
         [HttpGet]
+        [Authorize]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
@@ -16,6 +18,7 @@ namespace Server.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
+        [Authorize("Admin")]
         public ActionResult<string> Get(int id)
         {
             return "value";
--- server/Server/Startup.cs
+++ server/Server/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +19,30 @@ namespace Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = "https://dotnetgroup.eu.auth0.com";
+                options.Audience = "https://com.dotnetgroup";
+            });
+            services.AddAuthorization(options => {
+                options.AddPolicy("Admin", policy => policy.RequireClaim("https://com.dotnetgroup/roles", "admin"));
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("ClientApp",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin().AllowAnyHeader();
+                        builder.AllowAnyOrigin().AllowAnyMethod();
+                    });
+            });
         }
         
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -31,6 +56,8 @@ namespace Server
                 app.UseHsts();
             }
 
+            app.UseCors("ClientApp");
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
